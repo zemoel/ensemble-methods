eab1566e5563aff6aab014fb3bffd2912707eafe
Misgana <misgananegassi@gmail.com>
2016-05-31 16:15:36 +0200

    building matrix from predictions- naive

--- src/main/java/Stacking.java
+++ src/main/java/Stacking.java
@@ -1,3 +1,4 @@
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.util.MLUtils;
@@ -6,6 +7,7 @@ import scala.Tuple2;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pekasa on 05.05.16.
@@ -22,12 +24,11 @@ public class Stacking {
         JavaRDD<LabeledPoint> testing_data = datasets._2();
         ArrayList<String> models = new ArrayList<String>(2);
         models.add("DECISION_TREE");
+        models.add("RANDOM_FOREST_CLASSIFICATION");
         s.train(training_data, models);
 
         //summary = s.test(testing_data, model);
 
-        //s.train(set, models);
-        //
 
     }
     public Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>> preProcess() throws IOException {
@@ -47,31 +48,64 @@ public class Stacking {
      * @return
      */
 
-    public void train(JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels, Integer numFolds, Integer seed){
+    public void train(JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels, Integer numFolds, Integer seed) throws  NullPointerException{
+
 
         RDD<LabeledPoint> r = trainDataset.rdd();
-        ReadCSV read = new ReadCSV();
         ReadCSV build = new ReadCSV();
 
-        Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>>[] folds =  MLUtils.kFold(r, numFolds, seed, trainDataset.classTag());
-        // TODO: For every Fold train and test a model passed as list of model, combine predictions, compute alpha
-        for (Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>> fold: folds) {
 
-            for (String model: baseModels) {
+        Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>>[] folds =  MLUtils.kFold(r, numFolds, seed, trainDataset.classTag());
+        JavaPairRDD<Double, Double> modelPredictions = null;
+        List<Double> predictions = null;
+        ArrayList<ArrayList<Double>> matrix = new ArrayList<ArrayList<Double>>();
+
+
+        double[][] level1Dataset = new double[(int)trainDataset.count()][baseModels.size()];//TODO: Cast not safe
+
+        // TODO: For every Fold train and test a model passed as list of model, combine predictions, compute alpha
+        for (String model: baseModels) {
+            for (Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>> fold: folds) {
+
+
                 try {
-                    build.buildBaseModels(model, fold._1().toJavaRDD(), fold._2().toJavaRDD());
+                     modelPredictions =  build.buildBaseModels(model, fold._1().toJavaRDD(),
+                            fold._2().toJavaRDD());
+
+
+
+
+                    modelPredictions.join(modelPredictions);
+                    predictions = modelPredictions.keys().collect();
+                    matrix.add((ArrayList<Double>) predictions);
+
+                    //TODO: There must be a way to convert javapairrdd to matrix
+
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
 
 
         }
+        for(int i= 0; i<matrix.size(); i++){
+            ArrayList<Double> row = matrix.get(i);
+            double[] copy = new double[row.size()];
+            for(int j = 0; j< copy.length; j++){
+                copy[j] = row.get(j);
+            }
+            level1Dataset[i] = copy;
+
+        }
+
+
 
     }
     public void train(JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels){
 
-        train(trainDataset,baseModels,6, 12345);
+        train(trainDataset,baseModels,2, 12345);
 
     }
 
