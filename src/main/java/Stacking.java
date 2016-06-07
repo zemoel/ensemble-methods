2aee80c5fa27a3e750d3b91402fb151ea9304d7e
Misgana <misgananegassi@gmail.com>
2016-06-07 14:51:33 +0200

    Stacking using randomForest as basemodel

--- src/main/java/Stacking.java
+++ src/main/java/Stacking.java
@@ -6,11 +6,15 @@ import org.apache.spark.mllib.util.MLUtils;
 import org.apache.spark.rdd.RDD;
 import org.wso2.carbon.ml.commons.constants.MLConstants;
 import org.wso2.carbon.ml.commons.domain.MLModel;
+import org.wso2.carbon.ml.core.exceptions.MLModelBuilderException;
 import org.wso2.carbon.ml.core.exceptions.MLModelHandlerException;
 import org.wso2.carbon.ml.core.impl.Predictor;
+import org.wso2.carbon.ml.core.interfaces.MLModelBuilder;
+import org.wso2.carbon.ml.core.internal.MLModelConfigurationContext;
 import org.wso2.carbon.ml.core.spark.models.MLRandomForestModel;
 import scala.Tuple2;
 
+import java.io.Externalizable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +22,33 @@ import java.util.List;
 /**
  * Created by pekasa on 01.06.16.
  */
-public class Stacking {
-    public static void main(String[] args) throws IOException, MLModelHandlerException {
-        Stacking s = new Stacking();
+public class Stacking extends MLModelBuilder{
+    public Stacking(MLModelConfigurationContext context) {
+        super(context);
+    }
+
+    public MLModel build() throws MLModelBuilderException {
+        return null;
+    }
+
+    public MLModel buildBaseModels(JavaRDD<LabeledPoint> CvTrainingData) throws MLModelBuilderException {
+        ReadCSV buildbasemodel = new ReadCSV();
+        MLModelConfigurationContext context = getContext();
+        MLModel ml = new MLModel();
+        ml.setAlgorithmName(MLConstants.SUPERVISED_ALGORITHM.RANDOM_FOREST_CLASSIFICATION.toString());
+        ml.setEncodings(context.getEncodings());
+        ml.setAlgorithmClass("Classification");
+        MLRandomForestModel mlRandomForest  = new MLRandomForestModel();
+        RandomForestModel randomForestModel = buildbasemodel.buildRandomForest(CvTrainingData);
+        mlRandomForest.setModel(randomForestModel);
+        ml.setModel((Externalizable) mlRandomForest);
+
+        return ml;
+    }
+
+    public static void main(String[] args) throws IOException, MLModelHandlerException, MLModelBuilderException {
+        MLModelConfigurationContext context = null;
+        Stacking s = new Stacking(context);
         Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>> datasets = s.preProcess();
 
        JavaRDD<LabeledPoint> training_data = datasets._1();
@@ -28,13 +56,6 @@ public class Stacking {
         ArrayList<String> models = new ArrayList<String>(2);
         models.add("DECISION_TREE");
         models.add("RANDOM_FOREST_CLASSIFICATION");
-
-
-        MLModel ml = new MLModel();
-        ml.setAlgorithmName(MLConstants.SUPERVISED_ALGORITHM.RANDOM_FOREST_CLASSIFICATION.toString());
-        MLRandomForestModel rf = new MLRandomForestModel();
-        ml.setModel(rf);
-        ml.setAlgorithmClass("Classification");
 
         long modelId = 2;
         s.train(training_data, models);
@@ -46,8 +67,6 @@ public class Stacking {
 
     }
 
-
-
     public Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>> preProcess() throws IOException {
         ReadCSV read = new ReadCSV();
         JavaRDD<LabeledPoint> inputData = read.readCSV();
@@ -55,6 +74,8 @@ public class Stacking {
 
         return new Tuple2<JavaRDD<LabeledPoint>, JavaRDD<LabeledPoint>>(tmp[0], tmp[1]);
     }
+
+
 
 
     /**
@@ -67,18 +88,12 @@ public class Stacking {
      * @return
      */
 
-    public void train( long modelId,JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels, Integer numFolds, Integer seed) throws  NullPointerException, MLModelHandlerException {
+    public void train( long modelId,JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels, Integer numFolds, Integer seed) throws NullPointerException, MLModelHandlerException, MLModelBuilderException {
 
 
         RDD<LabeledPoint> r = trainDataset.rdd();
-        ReadCSV build = new ReadCSV();
+        //ReadCSV build = new ReadCSV();
         ReadCSV convert = new ReadCSV();
-        MLModel ml = new MLModel();
-        ml.setAlgorithmName(MLConstants.SUPERVISED_ALGORITHM.RANDOM_FOREST_CLASSIFICATION.toString());
-        MLRandomForestModel rf = new MLRandomForestModel();
-        ml.setModel(rf);
-        ml.setAlgorithmClass("Classification");
-
 
 
         Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>>[] folds =  MLUtils.kFold(r, numFolds, seed, trainDataset.classTag());
@@ -90,25 +105,22 @@ public class Stacking {
         // TODO: For every Fold train and test a model passed as list of model, combine predictions, compute alpha
         for (String model: baseModels) {
              for (Tuple2<RDD<LabeledPoint>, RDD<LabeledPoint>> fold: folds) {
-                try {
 
-                    modelPredictions =  build.buildBaseModels(model, fold._1().toJavaRDD(),fold._2().toJavaRDD());
-                    JavaRDD<LabeledPoint> validataionData = fold._2().toJavaRDD();
-                    List<String[]> dataTobePredicted = convert.LabeledpointToListStringArray(validataionData);
-                    System.out.print(dataTobePredicted);
-                    RandomForestModel randomForestModel =  build.buildRandomForest(fold._1().toJavaRDD());
-
-                    Predictor predictor = new Predictor(modelId, ml,dataTobePredicted );
-                    List<?> predictions = predictor.predict();
-                    System.out.println(predictions);
+                 //modelPredictions =  build.buildBaseModels(model, fold._1().toJavaRDD(),fold._2().toJavaRDD());
+                 JavaRDD<LabeledPoint> validationData = fold._2().toJavaRDD();
+                 List<String[]> dataTobePredicted = convert.LabeledpointToListStringArray(validationData);
+                 System.out.print(dataTobePredicted);
+                 MLModel basemodel = buildBaseModels(fold._1.toJavaRDD());
 
 
-                    //TODO: There must be a way to convert javapairrdd to matrix
+                 Predictor predictor = new Predictor(modelId, basemodel,dataTobePredicted );
+                 List<?> predictions = predictor.predict();
+                 System.out.println(predictions);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+
+                 //TODO: There must be a way to convert javapairrdd to matrix
+
+             }
 
 
         }
@@ -119,7 +131,7 @@ public class Stacking {
 
     }
 
-    public void train( JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels) throws MLModelHandlerException {
+    public void train( JavaRDD<LabeledPoint> trainDataset, ArrayList<String> baseModels) throws MLModelHandlerException, MLModelBuilderException {
 
         train(2, trainDataset,baseModels,2, 12345);
 
